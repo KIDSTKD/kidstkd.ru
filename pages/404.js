6b8bd9323d0fe161a7017d0e8df09606056cb5f4
KIDSTKD <kidstkd.ru@gmail.com>
2022-11-05 14:48:08 +1000

    1
    1

---
+++ pages/404.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+import Heading from '../components/Heading'
+
+const Home = () => (
+  <>
+  <Head>
+    <title>Ошибка 404</title>
+  </Head>
+  <Heading text="Ошибка 404" />
+    <p>Неизвестная ошибка</p>
+  </>
+)
+
+export default Home
