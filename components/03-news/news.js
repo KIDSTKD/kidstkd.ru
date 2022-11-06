64e3bad05bf379019e08f81d5988c0735c1d8688
KIDSTKD <kidstkd.ru@gmail.com>
2022-11-06 23:41:49 +1000

    link on news

--- components/03-news/news.js
+++ components/03-news/news.js
@@ -1,19 +1,27 @@
 import styles from "../03-news/NewsBlock.module.css"
 import Image from "next/image";
+import Link from "next/link";
 
 import News22 from "../03-news/data/2022.js"
 
 
 const NewsBlock = () => {
     return (
-        <>        
-        {News22.map(({id, path, title, p}) => (
-        <div className={styles.news} key={id}>   
+        <> 
+               
+        {News22.map(({id, path, title, p, link}) => (
+            
+            <Link key={id} href={link}> 
+
+        <div className={styles.news}>
+            
             <div className={styles.news_img}><Image src={path} objectFit='cover' layout='fill'/></div>
             <div className={styles.news_header}>{title}</div>
-            <div className={styles.news_p}><p>{p}</p></div>
-            
-        </div>))}
+            <div className={styles.news_p}><p>{p}</p></div> 
+        </div>
+        </Link>
+
+        ))}
 
         </>
     );
