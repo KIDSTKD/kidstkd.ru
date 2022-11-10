3ddae75b5b623a8e41928f7c8c79b757034b6794
KIDSTKD <kidstkd.ru@gmail.com>
2022-11-10 23:38:46 +1000

    2.0.1

--- components/layout/logo/Logo.js
+++ components/layout/logo/Logo.js
@@ -1,9 +1,14 @@
 import styles from "../logo/Logo.module.css"
 import Link from "next/link";
+import Image from "next/image";
+
+
 const Logo = () => (
     <>
     <Link href="/">
-        <div className={styles.logo}> </div>
+        <div className={styles.logo}>
+        <Image src='/img/logo.png' objectFit='contain' layout='fill'/>
+        </div>
     </Link>
     </>
 );
