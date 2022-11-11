bc944d7fc6836e994146333aa48f9acbc63c361d
KIDSTKD <kidstkd.ru@gmail.com>
2022-11-11 17:35:20 +1000

    1

--- components/layout/mobilefooter/MobileFooter.js
+++ components/layout/mobilefooter/MobileFooter.js
@@ -5,9 +5,9 @@ import Link from "next/link";
 
 
 const navigation = [
-    { id: 1, title: 'home', title2: 'home_pressed', path: '/' },
+    // { id: 1, title: 'menu', title2: 'menu_pressed', path: '/' },
     { id: 2, title: 'bookmark', title2: 'bookmark_pressed', path: '/01_about' },
-    { id: 3, title: 'menu', title2: 'menu_pressed', path: '/02' },
+    { id: 3, title: 'home', title2: 'home_pressed', path: '/' },
     { id: 4, title: 'search', title2: 'search_pressed', path: '/03' },
     { id: 5, title: 'settings', title2: 'settings_pressed', path: '/04' }
 ];
@@ -21,13 +21,14 @@ const MobileFooter = () => {
         <>
 
             <div className={styles.mobile_footer}>
-                    
+
+                <div className={styles.menu}></div>
+    
                 {navigation.map(({ id, title, title2, path }) => (
-                    
+
                     <Link key={id} href={path}>
                         <div className={pathname === path ? styles[title2] : styles[title]}></div>
                     </Link>
-
                 ))}
             </div>
 
