ff2679967c161d976764bfff91985bbf2d177f3b
KIDSTKD <kidstkd.ru@gmail.com>
2022-11-24 23:17:18 +1000

    1

--- components/layout/logo/Logo.js
+++ components/layout/logo/Logo.js
@@ -2,12 +2,15 @@ import styles from "../logo/Logo.module.css"
 import Link from "next/link";
 import Image from "next/legacy/image";
 
+import Logoimg from '../../../public/img/logo.png'
+
+
 
 const Logo = () => (
     <>
     <Link href="/">
         <div className={styles.logo}>
-        <Image src='/img/logo.png' objectFit='contain' layout='fill' alt="Тхэквондо в Уссурийске"/>
+        <Image src={Logoimg} alt="Тхэквондо в Уссурийске"/>
         </div>
     </Link>
     </>
