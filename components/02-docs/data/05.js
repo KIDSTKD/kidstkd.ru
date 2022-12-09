02cb7451925f4f9824dcbedf18d732da853da15c
KIDSTKD <kidstkd.ru@gmail.com>
2022-12-09 19:42:03 +1000

    up

--- components/02-docs/data/05.js
+++ components/02-docs/data/05.js
@@ -6,7 +6,7 @@ const Docs02 = [
         p: 'KPNP', 
         date: '11.10.2022',
         number: ' 2.0.14',
-        link: '/docs/TKDScoring_setup(Ver2.0.14a_1110)_EN.exe'
+        link: 'https://t.me/c/1259400119/30'
     },
 
     { 
@@ -24,7 +24,7 @@ const Docs02 = [
         p: 'KPNP', 
         date: '31.05.2022',
         number: ' 2.0.7',
-        link: '/docs/TKDScoring_setup(Ver2.0.7Beta_0531)_EN.exe'
+        link: 'https://t.me/c/1259400119/29'
     },
 
     { 
@@ -34,6 +34,15 @@ const Docs02 = [
         date: '17.11.2022',
         number: '3.2.2',
         link: '/docs/tkStrikeGen2-3.2.2.pkg'
+    },
+
+    { 
+        id: 1, 
+        img: '/img/02/05/daedo.png', 
+        p: 'Daedo GEN1 Release, Macos', 
+        date: '',
+        number: '3.1.12',
+        link: 'https://t.me/c/1259400119/32'
     },
 
 
