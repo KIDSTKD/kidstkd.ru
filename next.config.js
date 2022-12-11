035bfaa0a46ca90411a0d97c07a175c39831b2f1
KIDSTKD <kidstkd.ru@gmail.com>
2022-12-11 22:47:49 +1000

    1

--- next.config.js
+++ next.config.js
@@ -5,3 +5,15 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/pages/01/03/:path*',
+        destination: '/01-about-us/03',
+        permanent: false
+      }
+    ]
+  },
+}
--- pages/_middleware.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse, NextRequest } from 'next/server'
-export async function middleware(req, ev) {
-    const { pathname } = req.nextUrl
-    if (pathname == '/pages/01') {
-        return NextResponse.redirect('/01-about')
-    }
-    return NextResponse.next()
-}
