f9d0431eb4807020b93c032ffd7421f31d80d71c
KIDSTKD <kidstkd.ru@gmail.com>
2022-12-11 22:26:43 +1000

    1

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -2,7 +2,7 @@ import { NextResponse, NextRequest } from 'next/server'
 export async function middleware(req, ev) {
     const { pathname } = req.nextUrl
     if (pathname == '/pages/01') {
-        return NextResponse.redirect('/hello-nextjs')
+        return NextResponse.redirect('/01-about')
     }
     return NextResponse.next()
 }
