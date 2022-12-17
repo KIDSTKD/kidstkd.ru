8c082472c64b33a67060a2db22d8f460532982ab
KIDSTKD <kidstkd.ru@gmail.com>
2022-12-17 22:07:19 +1000

    1

--- pages/03-news/2022/12-09-12.js
+++ pages/03-news/2022/12-09-12.js
@@ -14,13 +14,13 @@ const News1 = () => (
   <>
    <Head>
     <title>{EventTitle}</title>
-    <meta property="og:url" content={EventURL} />
-    <meta property="og:title" content={EventTitle} />
-    <meta property="og:type" content="article" />
-    <meta property="og:description" content={Event.p} />
-    <meta property="og:image" content={EventImg} />
-    
+    <meta property="og:url" content={EventURL}  key="url" />
+    <meta property="og:title" content={EventTitle}  key="title" />
+    <meta property="og:type" content="article"  key="type" />
+    <meta property="og:description" content={Event.p}  key="description"/>
+    <meta property="og:image" content={EventImg}  key="image" />
    </Head>
+
    <h1>{Event.date} - {Event.title}, {Event.place}</h1>
     <div className={styles.news_logo}>
         <Image src={Event.path} objectFit='cover' layout='fill' alt={Event.title}/>
--- pages/_document.js
+++ pages/_document.js
@@ -4,12 +4,19 @@ export default function Document() {
   return (
     <Html>
       <Head>
+
         <meta name="msapplication-TileColor" content="#255d98" />
         <meta name="theme-color" content="#255d98" /> 
-        <meta name="description" content="Тхэквондо в Уссурийске"/>
-
+        <meta name="description" content="Тхэквондо в Уссурийске" key="description"/>
 
         <meta property="og:site_name" content="KIDSTKD - ТХЭКВОНДО в УССУРИЙСКЕ" key="sitename"/>
+        <meta property="og:url" content="https://kidstkd.ru"  key="url" />
+        <meta property="og:title" content="Тхэквондо в Уссурийске"  key="title" />
+        <meta property="og:type" content="website"  key="type" />
+        <meta property="og:description" content="Мы приглашаем детей от 4 лет на занятия олимпийским видом спорта. Занятия проходят в мини-группах детей одного возраста, что обеспечивает положительный результат занятий. Занятия охватывают все аспекты тхэквондо, и включают в себя базовую подготовку, обучение ударной технике, обучение техническому комплексу пхумсэ."  key="description"/>
+        <meta property="og:image" content="https://kidstkd.ru/img/logo_black.png"  key="image" />
+           
+        
 
       </Head>
       <body>
