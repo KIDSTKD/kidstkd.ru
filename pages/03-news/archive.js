48a29fd96a0c4edecc83a39809784374fdeb18f6
KIDSTKD <kidstkd.ru@gmail.com>
2022-12-20 00:34:37 +1000

    1

--- pages/03-news/archive.js
+++ pages/03-news/archive.js
@@ -8,7 +8,7 @@ const News = () => (
    <Head>
     <title>KIDSTKD - Новости - Архив</title>
    </Head>
-    <Heading text="KIDSTKD - Новости - Архив" />
+    <Heading text="Новости - Архив" />
 
     <p><Link href={'../../../03-news/2022'}>Новости 2022</Link></p>
     <p><Link href={'../../../03-news/2021'}>Новости 2021</Link></p>
--- pages/04-calendar/archive.js
+++ pages/04-calendar/archive.js
@@ -7,8 +7,9 @@ const About = () => (
   <Head>
     <title>KIDSTKD - Календарь - Архив</title>
   </Head>
-  <Heading text="Архив" />
+  <Heading text="Календарь - Архив" />
 
+    <p><Link href={'../../../04-calendar/2023'}>Архив 2023</Link></p>
     <p><Link href={'../../../04-calendar/2022'}>Архив 2022</Link></p>
     <p><Link href={'../../../03-calendar/2021'}>Архив 2021</Link></p>
     <p><Link href={'../../../03-calendar/2020'}>Архив 2020</Link></p>
