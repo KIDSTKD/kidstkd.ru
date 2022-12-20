ef4ba0abf8c3f0dd8bd8cbb6acd64d0a04b3e7ee
KIDSTKD <kidstkd.ru@gmail.com>
2022-12-20 13:02:08 +1000

    1

---
+++ pages/03-news/2023.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+import Heading from '../../components/Heading'
+import NewsBlock from '../../components/03-news/news23'
+
+const News = () => (
+  <>
+   <Head>
+    <title>KIDSTKD - Новости 2023</title>
+   </Head>
+    <Heading text="Новости 2023" />
+    <NewsBlock />
+
+  
+
+  </>
+)
+
+export default News
