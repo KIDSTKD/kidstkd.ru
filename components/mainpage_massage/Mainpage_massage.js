b9e76640c7dbfe12e3614b9f36c78dc71157ad5d
KIDSTKD <kidstkd.ru@gmail.com>
2023-01-02 00:40:25 +1000

    1

---
+++ components/mainpage_massage/Mainpage_massage.js
@@ -0,0 +1,16 @@
+import styles from "../mainpage_massage/Mainpage_massage.module.css"
+
+
+const Massage = () => (
+    <>
+        <div className={styles.massage_block}>
+            <div className={styles.header}>Напиши нам</div>
+
+        </div>
+    </>
+
+
+
+);
+
+export default Massage
