6250575ad15003b07e5b95fd630dda2ddc740184
KIDSTKD <kidstkd.ru@gmail.com>
2023-01-03 13:15:23 +1000

    1

--- components/mainpage_slider/athlets/athlet.js
+++ components/mainpage_slider/athlets/athlet.js
@@ -1,6 +1,6 @@
 import styles from "../athlets/Athlet.module.css"
 import Link from "next/link";
-import Image from "next/image";
+import Image from "next/legacy/image";
 
 import AthletsData from "./AthletsData";
 
