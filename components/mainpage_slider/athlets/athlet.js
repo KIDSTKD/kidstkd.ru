188828feabc36a495a4fb684600aabd1d174bb60
KIDSTKD <kidstkd.ru@gmail.com>
2023-01-03 13:17:15 +1000

    Revert "1"
    This reverts commit 6250575ad15003b07e5b95fd630dda2ddc740184.

--- components/mainpage_slider/athlets/athlet.js
+++ components/mainpage_slider/athlets/athlet.js
@@ -1,6 +1,6 @@
 import styles from "../athlets/Athlet.module.css"
 import Link from "next/link";
-import Image from "next/legacy/image";
+import Image from "next/image";
 
 import AthletsData from "./AthletsData";
 
