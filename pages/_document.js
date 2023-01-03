5c3c55ff34cd655226acfc12e0c613ae88d9346e
KIDSTKD <kidstkd.ru@gmail.com>
2023-01-03 12:50:57 +1000

    1

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang='ru'>
       <Head>
 
         <meta name="msapplication-TileColor" content="#255d98" />
