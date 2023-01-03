4bfa1fc7ec76069a4e2f7875c160855e12282ede
KIDSTKD <kidstkd.ru@gmail.com>
2023-01-03 13:01:36 +1000

    1

--- next.config.js
+++ next.config.js
@@ -4,41 +4,39 @@ const nextConfig = {
   swcMinify: true,
 }
 
-module.exports = nextConfig
+export default nextConfig
 
-module.exports = {
-  async redirects() {
-    return [
-      { source: '/pages/01/03.html', destination: '/01-about-us/belttest', permanent: false },
-      { source: '/pages/01/03_10-9.html', destination: '/01-about-us/belttest', permanent: false },
-      { source: '/pages/01/03_8.html', destination: '/01-about-us/belttest', permanent: false },
-      { source: '/pages/01/03_7.html', destination: '/01-about-us/belttest', permanent: false },
-      { source: '/pages/01/03_6.html', destination: '/01-about-us/belttest', permanent: false },
-      { source: '/pages/01/03_5.html', destination: '/01-about-us/belttest', permanent: false },
-      { source: '/pages/01/03_4.html', destination: '/01-about-us/belttest', permanent: false },
-      { source: '/pages/01/03_3.html', destination: '/01-about-us/belttest', permanent: false },
-      { source: '/pages/01/03_2.html', destination: '/01-about-us/belttest', permanent: false },
-      { source: '/pages/01/03_1.html', destination: '/01-about-us/belttest', permanent: false },
+export async function redirects() {
+  return [
+    { source: '/pages/01/03.html', destination: '/01-about-us/belttest', permanent: false },
+    { source: '/pages/01/03_10-9.html', destination: '/01-about-us/belttest', permanent: false },
+    { source: '/pages/01/03_8.html', destination: '/01-about-us/belttest', permanent: false },
+    { source: '/pages/01/03_7.html', destination: '/01-about-us/belttest', permanent: false },
+    { source: '/pages/01/03_6.html', destination: '/01-about-us/belttest', permanent: false },
+    { source: '/pages/01/03_5.html', destination: '/01-about-us/belttest', permanent: false },
+    { source: '/pages/01/03_4.html', destination: '/01-about-us/belttest', permanent: false },
+    { source: '/pages/01/03_3.html', destination: '/01-about-us/belttest', permanent: false },
+    { source: '/pages/01/03_2.html', destination: '/01-about-us/belttest', permanent: false },
+    { source: '/pages/01/03_1.html', destination: '/01-about-us/belttest', permanent: false },
 
-      {
-        source: '/pages/05/01/:path*',
-        destination: '/05-taekwondo/articles',
-        permanent: false
-      },
+    {
+      source: '/pages/05/01/:path*',
+      destination: '/05-taekwondo/articles',
+      permanent: false
+    },
 
-      {
-        source: '/pages/05/09.html',
-        destination: '/05-taekwondo/rules',
-        permanent: false
-      },
+    {
+      source: '/pages/05/09.html',
+      destination: '/05-taekwondo/rules',
+      permanent: false
+    },
 
-      {
-        source: '/pages/05/:path*',
-        destination: '/05-taekwondo/kibon',
-        permanent: false
-      },
-    ]
-  },
+    {
+      source: '/pages/05/:path*',
+      destination: '/05-taekwondo/kibon',
+      permanent: false
+    },
+  ]
 }
 
 
