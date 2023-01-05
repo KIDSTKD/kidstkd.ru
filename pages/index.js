8448d6964bde0a7072dda836ea7aa7e57e0130c9
KIDSTKD <kidstkd.ru@gmail.com>
2023-01-05 11:47:58 +1000

    Adding Blog on index page

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,20 @@
 import Head from 'next/head'
 
+import fs from 'fs';
+import matter from 'gray-matter';
+
 import Heading from '../components/Heading'
 import Slider from '../components/mainpage_slider/Mainpage_slider'
 import NewsBlock from '../components/03-news/newsIndex'
 import Link from 'next/link'
 
+import Image from 'next/image';
+import styles from '../components/05-taekwondo/articles/ArticlesBlock.module.css'
 
 
 
-const Home = () => (
+
+const Home = ({posts}) => (
   <>
   <Head>
     <title>KIDSTKD - Тхэквондо в Уссурийске</title>
@@ -34,10 +40,51 @@ const Home = () => (
   <NewsBlock />
     <Link href='/05-taekwondo/blog' className='link_h4'><h4>Публикации</h4></Link>
 
+
+    <div className={styles.all}>
+        {posts.map(post => {
+            const {slug, frontmatter} = post
+            const {title, bannerImage, description} = frontmatter
+
+            return <article key={title}>
+                
+                <Link href={`/posts/${slug}`}>
+                    <div className={styles.articles2}>
+                    <div className={styles.articles_img2}><Image className={styles.blog_image} src={bannerImage} fill='true' /></div>
+                    <h2 className={styles.articles_header2}>{title}</h2>
+                    <p className={styles.articles_p2}>{description}</p>
+                    </div>
+                </Link>
+                
+            </article>
+        })}
+    </div>
     
-
-
   </>
 )
 
 export default Home
+
+
+
+export async function getStaticProps(){
+  const files = fs.readdirSync('posts');
+  const filesRev = files.reverse();
+  const files9 = filesRev.slice(-0,6)
+  const posts = files9.map((fileName) => {
+      const slug = fileName.replace('.md', '');
+      const readFile = fs.readFileSync(`posts/${fileName}`, 'utf-8');
+      const { data: frontmatter } = matter(readFile);
+
+      return {
+        slug,
+        frontmatter,
+      };
+  });
+
+  return {
+      props: {
+        posts,
+      },
+  };
+}
