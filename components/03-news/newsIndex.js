a55d2f26495fc7f33370be47fce6d6e6939a52fa
KIDSTKD <kidstkd.ru@gmail.com>
2023-01-10 13:15:31 +1000

    NEWS markdown

--- components/03-news/newsIndex.js
+++ components/03-news/newsIndex.js
@@ -2,13 +2,10 @@ import styles from "../03-news/NewsBlock.module.css"
 import Image from "next/legacy/image";
 import Link from "next/link";
 
-import News23 from "./data/2023.js"
 import News22 from "./data/2022.js"
 
-const NewsAll = [...News23, ...News22];
 
-
-let NewsLast6 = NewsAll.slice(-0,6)
+let NewsLast6 = News22.slice(-0,6)
 
 
 const NewsBlock = () => {
--- pages/03-news/2019.js
+++ pages/03-news/2019.js
@@ -1,19 +1,62 @@
 import Head from 'next/head'
-
 import Heading from '../../components/Heading'
-import NewsBlock from '../../components/03-news/news19'
 
-const News = () => (
-  <>
-   <Head>
+import fs from 'fs';
+import matter from 'gray-matter';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import styles from '../../components/03-news/NewsBlock.module.css'
+
+
+const News = ({posts}) => {
+
+  return  <>
+  
+  <Head>
     <title>KIDSTKD - Новости 2019</title>
    </Head>
     <Heading text="Новости 2019" />
-    <NewsBlock />
 
-  
+    <div>
+      {posts.map(a => {
+          const {slug, frontmatter} = a
+          const {title, date, place, bannerImage, description} = frontmatter
+          return <article key={title}>
+              
+              <Link href={`/posts/news/${slug}`}>
+                  <div className={styles.news}>
+                  <div className={styles.news_img}><Image className='img' src={bannerImage} alt={title} fill='true' /></div>
+                  <div className={styles.news_header}>{date} - {title} ({place})</div>
+                  <p className={styles.news_p}>{description}</p>
+                  </div>
+              </Link>
+              
+          </article>
+      })}
+    </div></>
+    }
 
-  </>
-)
+export default News;
 
-export default News
+export async function getStaticProps(){
+  const files = fs.readdirSync('posts/news');
+  const filesRev = files.reverse();
+  const filesFiltred = filesRev.filter(date => date.includes(2019))
+  const posts = filesFiltred.map((fileName) => {
+      const slug = fileName.replace('.md', '');
+      const readFile = fs.readFileSync(`posts/news/${fileName}`, 'utf-8');
+      const { data: frontmatter } = matter(readFile);
+
+      return {
+        slug,
+        frontmatter,
+      };
+  });
+
+  return {
+      props: {
+        posts,
+      },
+  };
+}
