2bd799c604d93c705898dc12feda79db2b533a48
KIDSTKD <kidstkd.ru@gmail.com>
2023-05-25 12:33:59 +1000

    BAKU 2023

---
+++ pages/baku-2023-live/01.js
@@ -0,0 +1,29 @@
+import Head from 'next/head'
+
+import Heading from '../../components/Heading'
+
+const About = () => (
+  <>
+  <Head>
+    <title>KIDSTKD - Прямая трансляция Чемпионата Мира по тхэквондо - День 1</title>
+  </Head>
+  <Heading text="Прямая трансляция Чемпионата Мира по тхэквондо - День 1" />
+
+
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/aFoArqQ9Sio" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/uFxLgPps5xw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/qdaOOV5fzIA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/nCNM9dc_648" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/kCbsn6J05jk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
+
+
+
+  </>
+)
+
+export default About
